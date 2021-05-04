24a56831556ba08642b1c1df1fa80de7d81cb990
Ansbert <ukush89@hotmail.com>
2021-05-04 21:31:47 +0930

    Initial Commit

---
+++ githubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubTest
+//
+//  Created by Ansbert Ndayisaba on 4/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubTest/githubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTestApp.swift
+//  githubTest
+//
+//  Created by Ansbert Ndayisaba on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct githubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
